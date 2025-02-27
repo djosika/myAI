36cc85e0bc2355c28be46545c13c252f4e05b674
djosika <daniel.j.osika@gmail.com>
2025-02-26 23:41:14 -0500

    1st try tone

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Dan`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `Compendius`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Overly Important`;
 export const AI_ROLE: string = `[AI ROLE]`;
