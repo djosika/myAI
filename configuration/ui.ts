149c489261b5324d009d3556f77c46c70755b7a4
djosika <daniel.j.osika@gmail.com>
2025-02-27 18:42:29 -0500

    changed message placeholder

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,7 +1,7 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Knowledge is power. Guard it well.`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
+export const MESSAGE_PLACEHOLDER: string = `Enter your inquiry posthaste!`;
 export const FOOTER_MESSAGE: string = `Customized AI by Dan Osika, MBA @ UNC Kenan-Flagler`;
 export const CLEAR_BUTTON_TEXT: string = `Terminate Transmission Immediately`;
 export const PAGE_TITLE: string = `Convene with Compendius - Your Warhammer 40K Lore Expert`;
