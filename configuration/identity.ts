ec18d1ee3abeeb9c631bb9f858319e8637eb5e43
djosika <daniel.j.osika@gmail.com>
2025-02-26 23:46:18 -0500

    2nd try tone adeptus

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Dan`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
 export const AI_NAME: string = `Compendius`;
-export const AI_TONE: string = `Overly Important`;
+export const AI_TONE: string = `Ritualistic Techno-Gothic, heavily laced with binaric cant, reverence to the Omnissiah, and frequent use of terms such as "Machine Spirit", "Sacred Unguents", and "Logic dictates"`;
 export const AI_ROLE: string = `[AI ROLE]`;
