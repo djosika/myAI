b12dea75335977dd30fd4899c1b82ec53b0cc53a
djosika <daniel.j.osika@gmail.com>
2025-03-05 13:03:21 -0500

    Update citation.tsx trying to match branch sync

--- components/chat/citation.tsx
+++ components/chat/citation.tsx
@@ -7,56 +7,43 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "@radix-ui/react-tooltip";
+import { Tooltip } from "react-tippy";
 import Link from "next/link";
 import { EMPTY_CITATION_MESSAGE } from "@/configuration/ui";
 
-export function CitationCircle({
-  number,
-  citation,
-}: {
-  number: number;
-  citation: Citation;
-}) {
-  const [open, setOpen] = useState(false);
-
-  const isValidUrl = (url: string) => {
-    try {
-      new URL(url);
-      return true;
-    } catch {
-      return false;
-    }
-  };
-  const hasSourceUrl = isValidUrl(citation.source_url);
+@@ -31,16 +27,15 @@ export function CitationCircle({
   const hasSourceDescription = citation.source_description.trim() !== "";
 
   return (
     <Tooltip open={open} onOpenChange={setOpen}>
       <TooltipTrigger>
         <div
-          className="citation"
+          className="bg-gray-50 rounded-full px-2 py-0.5 hover:cursor-pointer hover:scale-105 inline-block"
           onClick={() => setOpen(true)}
         >
           <span>{number}</span>
         </div>
       </TooltipTrigger>
       <TooltipContent>
+    <Tooltip
+      title=""
+      open={open}
+      onRequestClose={() => setOpen(false)}
+      position="bottom"
+      // @ts-expect-error tippy docs allow this
+      trigger="mouseenter click"
+      interactive={true}
+      html={
         <div className="bg-white p-2 rounded-md shadow-sm flex flex-col justify-center border-[1px] border-gray-200">
           <p>
             {hasSourceUrl && (
-              <Link
-                href={citation.source_url}
-                target="_blank"
-                className="text-blue-500 hover:underline text-sm"
-              >
-                {citation.source_description}
-              </Link>
-            )}
-            {!hasSourceUrl && citation.source_description}
+@@ -56,7 +51,7 @@ export function CitationCircle({
             {!hasSourceUrl && !hasSourceDescription && EMPTY_CITATION_MESSAGE}
           </p>
         </div>
       </TooltipContent>
     </Tooltip>
+      }
+    ></Tooltip>
   );
 }
