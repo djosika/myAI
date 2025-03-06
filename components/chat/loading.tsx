5fdabf2d423822b0d9a0f67cd3b687c332044e55
djosika <daniel.j.osika@gmail.com>
2025-03-06 17:49:51 -0500

    Update loading.tsx

--- components/chat/loading.tsx
+++ components/chat/loading.tsx
@@ -24,8 +24,8 @@ export function Pill({
 }) {
   return (
     <motion.div
-      className={`flex flex-row gap-2 items-center font-mono text-sm ${
-        isDone ? "text-green-300" : "text-green-300 animate-pulse"
+      className={`flex flex-row gap-2 items-center font-mono text-sm font-bold ${
+        isDone ? "text-green-500" : "text-green-500 animate-pulse"
       } ${isError ? "text-red-500" : ""}`}
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
@@ -39,7 +39,7 @@ export function Pill({
         <AlertCircle className="w-4 h-4 animate-pulse text-red-500" />
       )}
 
-      <p className="tracking-wide shadow-black text-lg">
+      <p className="tracking-wide shadow-black/90 text-lg">
         {icon === "thinking" && "[+] Cogitation-Rite Initiated..."}
         {icon === "searching" && "[~] Scanning Lexicanum..."}
         {icon === "understanding" && "[#] Deciphering Data-Wafers..."}
@@ -75,17 +75,17 @@ export default function Loading({
     <motion.div
       whileHover={{ scale: 1.01 }}
       transition={{ type: "spring", stiffness: 300, damping: 20 }}
-      className="py-1 max-w-[60%] transition-shadow duration-300 text-green-300 font-mono"
+      className="py-1 max-w-[60%] transition-shadow duration-300 text-green-500 font-mono font-bold"
     >
       {!bootUpComplete ? (
-        <div className="text-green-300 animate-fade-in">
+        <div className="text-green-500 animate-fade-in">
           {displayedMessages.map((msg, index) => (
             <motion.p
               key={index}
               initial={{ opacity: 0, x: -10 }}
               animate={{ opacity: 1, x: 0 }}
               transition={{ duration: 0.5, ease: "easeOut" }}
-              className="tracking-wide shadow-black text-lg"
+              className="tracking-wide shadow-black/90 text-lg"
             >
               {msg}
             </motion.p>
