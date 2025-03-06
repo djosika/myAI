e79c6eb7233b50baee40ba910fdcbc30f177609d
djosika <daniel.j.osika@gmail.com>
2025-03-06 16:09:39 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,7 +2,7 @@ import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `Knowledge is power. Guard it well.`;
 export const MESSAGE_PLACEHOLDER: string = `Enter your inquiry posthaste!`;
-export const FOOTER_MESSAGE: string = `Customized AI by Dan Osika, MBA @ UNC Kenan-Flagler`;
+export const FOOTER_MESSAGE: string = `Customized AI by Daniel Osika, MBA @ UNC Kenan-Flagler`;
 export const CLEAR_BUTTON_TEXT: string = `Terminate Transmission Immediately`;
 export const PAGE_TITLE: string = `Convene with Compendius - Your Warhammer 40K Lore Expert`;
 export const PAGE_DESCRIPTION: string = `Bring all your Warhammer 40K questions to ${AI_NAME}, ${OWNER_NAME}'s narrative assistant.`;
