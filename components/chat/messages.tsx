ab61bf716932081017a529b96d42a2702bf93b2d
djosika <daniel.j.osika@gmail.com>
2025-03-13 04:11:20 -0400

    Update messages.tsx

--- components/chat/messages.tsx
+++ components/chat/messages.tsx
@@ -9,7 +9,7 @@ import { AI_NAME } from "@/configuration/identity";
 function AILogo() {
   return (
     <div className="w-9 h-9">
-      <Imag src="/ai-logo.png" alt={AI_NAME} width="48" height="48" />
+      <Img src="/ai-logo.png" alt={AI_NAME} width="48" height="48" />
     </div>
   );
 }
